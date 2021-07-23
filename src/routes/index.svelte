5fb2bf03436688fcfc8674f601bf55e225737119
Johnny <johnny.jarecsni@icloud.com>
2021-07-23 17:04:54 +0100

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<script>
+    import {hello} from '@jarecsni/nodez';
+</script>
+<h1>Welcome to SvelteKit: {hello()}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
