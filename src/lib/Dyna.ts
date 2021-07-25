1a61b7c0c61af4586e566df7f5dab871560ce501
Johnny <johnny.jarecsni@icloud.com>
2021-07-25 10:35:01 +0100

    Further tests around async loading

--- src/lib/Dyna.ts
+++ src/lib/Dyna.ts
@@ -2,11 +2,13 @@ import type { Plugin } from "./plugins/Plugin";
 
 export class Dyna {
     plugins: { [key: string]: Plugin; } = {}; 
-    
+    moduleLoadPromise: Promise<Plugin>;
     async loadPlugin(name: string) {
-        const module = await import(`./plugins/${name}.ts`);
+        this.moduleLoadPromise = import(`./plugins/${name}.ts`);
+        const module = await this.moduleLoadPromise;
         const plugin = new module[name]();
         if (this.isPlugin(plugin)) {
+            this.plugins[name] = plugin;
             return plugin;
         } else {
             throw Error(`Module ${name} is not a Plugin`);
@@ -16,4 +18,12 @@ export class Dyna {
     isPlugin(module: any): module is Plugin {
         return module && module.execute && typeof(module.execute) === 'function';
     }
-}
+
+    async execute(name: string) {
+        if (!this.plugins[name]) {
+            this.loadPlugin(name);
+        }
+        await this.moduleLoadPromise;
+        this.plugins[name].execute();
+    }
+ }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,9 +6,16 @@
     import type {Plugin} from '$lib/plugins/Plugin';
 
     const dyna = new Dyna();
-    const m = dyna.loadPlugin('PluginOne').then(module => {
-        (module as Plugin).execute();    
-    })
+    // const m = dyna.loadPlugin('PluginOne').then(module => {
+    //     //(module as Plugin).execute(); 
+    //     dyna.execute('PluginOne');   
+    // })
+
+    // async function load(): Promise<Plugin> {
+    //     return await dyna.loadPlugin('PluginOne');
+    // }
+    // load();
+    dyna.execute('PluginOne');
 </script>
 <h1>Welcome to SvelteKit: {hello()}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
